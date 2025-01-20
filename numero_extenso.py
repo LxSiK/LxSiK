6cc3b8c2f9836413e502067083515e366d92a838
LxSiK <165629264+LxSiK@users.noreply.github.com>
2025-01-20 11:57:43 -0300

    Delete numero_extenso.py

--- numero_extenso.py
+++
@@ -1,7 +0,0 @@
-numero_extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez')
-while True:
-    numero = int(input('Digite um número: '))
-    if 0 <= numero <= 20:
-        break
-    print('Número entre 0 e 10.', end = ' ')
-print(f'Número digitado: {numero_extenso[numero]}.')
